70848d31100a892a7af93fe3dfdb12fb9f810d5c
toshi0607 <s.toshi0607@gmail.com>
2018-02-08 10:40:32 +0900

    :sparkles: 1.4

---
+++ ch1/1.4/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	filenames := make(map[string]map[string]int)
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts, filenames)
+	} else {
+		for _, file := range files {
+			f, err := os.Open(file)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts, filenames)
+			f.Close()
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			names, sep := "", ""
+			for name := range filenames[line] {
+				names += sep + name
+				sep = "/"
+			}
+			output :=
+				fmt.Printf("%d\t%s\n", n+" "+line+" "+filenames)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int, filenames map[string]map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		str := input.Text()
+		counts[str]++
+		if filenames != nil {
+			if filenames[str] == nil {
+				filenames[str] = make(map[string]int)
+			}
+			filenames[str][f.Name()]++
+		}
+	}
+}
