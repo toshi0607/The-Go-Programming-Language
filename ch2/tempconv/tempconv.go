b842d3b8dc089249350b2abf7d28da5e77fa36d8
toshi0607 <s.toshi0607@gmail.com>
2018-02-14 10:04:05 +0900

    :sparkles: tempconv

---
+++ ch2/tempconv/tempconv.go
@@ -0,0 +1,16 @@
+// Package tempconv は摂氏と華氏の温度変換を行います
+package tempconv
+
+import "fmt"
+
+type Celsius float64
+type Fahrenheit float64
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC     Celsius = 0
+	BoilingC              = 100
+)
+
+func (c Celsius) String() string    { return fmt.Sprintf("%g°C", c) }
+func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
