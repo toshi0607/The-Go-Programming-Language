9f2ca90fff987cc19b93e4ce96dd8183a81cc63c
toshi0607 <s.toshi0607@gmail.com>
2018-02-19 10:54:51 +0900

    :sparkles: graph

---
+++ ch4/graph/main.go
@@ -0,0 +1,14 @@
+var graph = make(map[string]map[string]bool)
+
+func edge(from, to string) {
+	edges := graph[from]
+	if edges == nil {
+		edges = make(map[string]bool)
+		graph[from] = edges
+	}
+	edges[to] = true
+}
+
+func hasEdge(from, to string) bool {
+	return graph[from][to]
+}
