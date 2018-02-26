f07a4d1866aa3862e4bf458bbf26fe9053a03c50
toshi0607 <s.toshi0607@gmail.com>
2018-02-26 10:25:01 +0900

    :sparkles: findlinks3

---
+++ ch5/findlinks3/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	links "github.com/toshi0607/plgo/ch5/findlinks"
+)
+
+func main() {
+	breadthFirst(crawl, os.Args[1:])
+}
+
+func breadthFirst(f func(item string) []string, worklist []string) {
+	seen := make(map[string]bool)
+	for len(worklist) > 0 {
+		items := worklist
+		worklist = nil
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				worklist = append(worklist, f(item)...)
+			}
+		}
+	}
+}
+
+func crawl(url string) []string {
+	fmt.Println(url)
+	list, err := links.Extract(url)
+	if err != nil {
+		log.Print(err)
+	}
+	return list
+}
