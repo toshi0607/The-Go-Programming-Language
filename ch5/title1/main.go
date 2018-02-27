280a166ec0b6e938d3260ff9e62a9041dd8b6bd4
toshi0607 <s.toshi0607@gmail.com>
2018-02-27 09:25:18 +0900

    :sparkles: title1

---
+++ ch5/title1/main.go
@@ -0,0 +1,26 @@
+func title(uel string) error {
+	resp, err := http:Get(url)
+	if err != nil {
+		return err
+	}
+	ct := resp.Header.Get("Content-Type")
+	if ct != "text/html" && !strings.HasPrefix(ct, "text/html;") {
+		resp.Body.Close()
+		return fmt.Errorf("%s has type %s, not text/html"m url, ct)
+	}
+
+	doc, err := html.Parse(resp.body)
+	resp.Body.Close()
+	if err != nil {
+		return fmt.Errorf("parsing %s as HTML: %v", url, err)
+	}
+
+	visitNode := func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "title" &&
+		  n.FirstChild != nil {
+				fmt.Println(n.FirstChild.Data)
+			}
+	}
+	forEachNode(doc, visitNode, nil)
+	return nil
+}
