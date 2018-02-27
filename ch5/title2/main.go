53b37529c6a6f17bcbb8613a1febeb08f95e98da
toshi0607 <s.toshi0607@gmail.com>
2018-02-27 09:38:00 +0900

    :sparkles: title2

---
+++ ch5/title2/main.go
@@ -0,0 +1,21 @@
+import "net/http"
+
+func title(url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	ct := resp.Header.Get("Content-type")
+	if ct != "text/html" && !strings.HasPrefix(ct, "text/html;") {
+		return fmt.Errorf("%s has type %s. not text/html", url, ct)
+	}
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return fmt.Errorf("parsing %s as HTML: %v", url, err)
+	}
+
+	return nil
+}
