39a0aa35d729e021d87138e03362631dd6478c95
toshi0607 <s.toshi0607@gmail.com>
2018-04-09 09:50:50 +0900

    :sparkles: netcat2

--- ch8/reverb1/main.go
+++ ch8/reverb1/main.go
@@ -29,7 +29,7 @@ func echo(c net.Conn, shout string, delay time.Duration) {
 	time.Sleep(delay)
 	fmt.Fprintln(c, "\t", shout)
 	time.Sleep(delay)
-	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
 }
 
 func handleConn(c net.Conn) {
